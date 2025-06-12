cf40403db1cd74f54b4aa542f248506b72c33bfb
Ylem <mateusz.romus@gmail.com>
2025-06-13 01:31:35 +0200

    fix deploy

--- src/components/textures.ts
+++ src/components/textures.ts
@@ -2,14 +2,15 @@ import * as THREE from 'three';
 
 export function loadTextures() {
   const loader = new THREE.TextureLoader();
+  const base = import.meta.env.BASE_URL;
 
   return {
-    earthDay: loader.load('/assets/2k_earth_daymap.jpg'),
-    earthNight: loader.load('/assets/2k_earth_nightmap.jpg'),
-    specularMap: loader.load('/assets/2k_earth_daymap.jpg'),
-    cloudMap: loader.load('/assets/2k_earth_clouds.jpg'),
-    sunTexture: loader.load('/assets/2k_sun.jpg'),
-    moonTexture: loader.load('/assets/2k_moon.jpg'),
-    fakeMoonTexture: loader.load('/assets/2k_makemake_fictional.jpg')
+    earthDay: loader.load(base + 'assets/2k_earth_daymap.jpg'),
+    earthNight: loader.load(base + 'assets/2k_earth_nightmap.jpg'),
+    specularMap: loader.load(base + 'assets/2k_earth_daymap.jpg'),
+    cloudMap: loader.load(base + 'assets/2k_earth_clouds.jpg'),
+    sunTexture: loader.load(base + 'assets/2k_sun.jpg'),
+    moonTexture: loader.load(base + 'assets/2k_moon.jpg'),
+    fakeMoonTexture: loader.load(base +'assets/2k_makemake_fictional.jpg')
   };
 }
